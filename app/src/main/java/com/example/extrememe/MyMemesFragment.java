8b505e5e19d01c39dd5d38a4304d0b22fb97a92f
Oz Zorany <ozzorany@gmail.com>
2020-12-17 15:22:39 +0200

    create my memes fragment

---
+++ app/src/main/java/com/example/extrememe/MyMemesFragment.java
@@ -0,0 +1,17 @@
+package com.example.extrememe;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MyMemesFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_memes, container, false);
+    }
+}
