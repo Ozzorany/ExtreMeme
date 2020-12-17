b0c81bfaa92ab3afa8454ac4a4a880a268fa03c1
Oz Zorany <ozzorany@gmail.com>
2020-12-17 15:24:21 +0200

    use the fragments in the main activity as bottom navbar, and update upper bar to display the current screen name

--- app/src/main/java/com/example/extrememe/MainActivity.java
+++ app/src/main/java/com/example/extrememe/MainActivity.java
@@ -1,19 +1,53 @@
 package com.example.extrememe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.extrememe.services.CategoryService;
-import com.example.extrememe.services.DatabaseDataLoader;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navController = Navigation.findNavController(this, R.id.mainactivity_navhost);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.mainactivity_navhost);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                getSupportActionBar().setTitle(destination.getLabel());
+            }
+        });
+
         new CategoryService().getMemeCategories();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            navController.navigateUp();
+            return true;
+        }
+
+        return NavigationUI.onNavDestinationSelected(item, navController);
     }
 }
