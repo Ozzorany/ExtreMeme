546d0f6aa85c4b9d48fed6f8aaac28b552dbc652
Oz Zorany <ozzorany@gmail.com>
2020-12-17 15:22:57 +0200

    create main feed fragment

---
+++ app/src/main/java/com/example/extrememe/MainFeedFragment.java
@@ -0,0 +1,19 @@
+package com.example.extrememe;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFeedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_feed, container, false);
+
+        return view;
+    }
+}
