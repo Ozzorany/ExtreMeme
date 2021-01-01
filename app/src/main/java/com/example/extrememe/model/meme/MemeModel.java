2d0fa640d0e11b0ea820b62a6717c45d8f0b13ed
Oz Zorany <ozzorany@gmail.com>
2021-01-01 21:36:45 +0200

    add remove meme option

--- app/src/main/java/com/example/extrememe/model/meme/MemeModel.java
+++ app/src/main/java/com/example/extrememe/model/meme/MemeModel.java
@@ -8,25 +8,38 @@ public class MemeModel {
     public final static MemeModel instance = new MemeModel();
     MemeModelFirebase memeModelFirebase = new MemeModelFirebase();
 
-    private MemeModel(){
+    private MemeModel() {
     }
 
-    public interface Listener<T>{
+    public interface Listener<T> {
         void onComplete(T result);
     }
 
-    public interface GetAllMemesListener extends Listener<List<Meme>>{}
-    public void getAllMemes(GetAllMemesListener listener){
+    public interface GetAllMemesListener extends Listener<List<Meme>> {
+    }
+
+    public void getAllMemes(GetAllMemesListener listener) {
         memeModelFirebase.getAllMemes(listener);
     }
 
-    public interface GetMemesByUserListener extends Listener<List<Meme>>{}
-    public void getMemesByUserId(String id, GetMemesByUserListener listener){
-        memeModelFirebase.getMemesByUserId(id,  listener);
+    public interface GetMemesByUserListener extends Listener<List<Meme>> {
     }
 
-    public interface UpdateMemeListener extends Listener<Void>{}
-    public void updateMeme(Meme meme, UpdateMemeListener listener){
+    public void getMemesByUserId(String id, GetMemesByUserListener listener) {
+        memeModelFirebase.getMemesByUserId(id, listener);
+    }
+
+    public interface UpdateMemeListener extends Listener<Void> {
+    }
+
+    public void updateMeme(Meme meme, UpdateMemeListener listener) {
         memeModelFirebase.updateMeme(meme, listener);
+    }
+
+    public interface RemoveMemeListener extends Listener<Void> {
+    }
+
+    public void removeMeme(String memeId, RemoveMemeListener listener) {
+        memeModelFirebase.removeMeme(memeId, listener);
     }
 }
