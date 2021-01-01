a1d0bcfc006a229f29fea4b49ffaed2a4b69a84b
Oz Zorany <ozzorany@gmail.com>
2021-01-01 14:16:58 +0200

    change tag name

--- app/src/main/java/com/example/extrememe/model/category/CategoryModelFirebase.java
+++ app/src/main/java/com/example/extrememe/model/category/CategoryModelFirebase.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryModelFirebase {
-    private static final String TAG = "MemesService";
+    private static final String TAG = "CategoryService";
 
     public void getMemeCategories(final CategoryModel.GetMemeCategoriesListener listener) {
         List<Category> list = new ArrayList<>();
