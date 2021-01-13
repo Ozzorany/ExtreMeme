e8db02f18a8ba1e7cc1aec83f2f62f58f8c6fe05
Oz Zorany <ozzorany@gmail.com>
2021-01-13 19:10:06 +0200

    listen to bottom nav items click, and validate if the user is logged in or not

--- app/src/main/java/com/example/extrememe/MainActivity.java
+++ app/src/main/java/com/example/extrememe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.extrememe;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -10,16 +11,19 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.extrememe.services.LoginService;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     NavController navController;
     public BottomNavigationView bottomNav;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
         navController = Navigation.findNavController(this, R.id.mainactivity_navhost);
         NavigationUI.setupActionBarWithNavController(this, navController);
@@ -30,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(bottomNav, navController);
 
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> getSupportActionBar().setTitle(destination.getLabel()));
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                boolean isUserLoggedIn = LoginService.getInstance(context).isLoggedIn();
+
+                switch (item.getItemId()) {
+                    case R.id.myMemesFragment:
+                        if (isUserLoggedIn) {
+                            Navigation.findNavController(MainActivity.this, R.id.mainactivity_navhost).navigate(R.id.myMemesFragment);
+                            return true;
+                        }
+                        return false;
+                    case R.id.createMemeFragment:
+                        if (isUserLoggedIn) {
+                            Navigation.findNavController(MainActivity.this, R.id.mainactivity_navhost).navigate(R.id.createMemeFragment);
+                            return true;
+                        }
+                        return false;
+                    case R.id.mainFeedFragment:
+                        Navigation.findNavController(MainActivity.this, R.id.mainactivity_navhost).navigate(R.id.mainFeedFragment);
+                        return true;
+                }
+
+                return true;
+            }
+        });
     }
 
     @Override
