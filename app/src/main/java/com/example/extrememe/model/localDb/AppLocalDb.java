8100b1524721f8cc20122281074745dd2a85b15c
Oz Zorany <ozzorany@gmail.com>
2021-01-15 22:39:54 +0200

    change db version

--- app/src/main/java/com/example/extrememe/model/localDb/AppLocalDb.java
+++ app/src/main/java/com/example/extrememe/model/localDb/AppLocalDb.java
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase;
 import com.example.extrememe.MemesApplication;
 import com.example.extrememe.model.Meme;
 
-@Database(entities = {Meme.class}, version = 2)
+@Database(entities = {Meme.class}, version = 3)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract MemeDao memeDao();
 }
