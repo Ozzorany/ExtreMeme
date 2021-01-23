a8607b8aa52c7730d7763763766728ba60351b15
yariv <yarivgavriel2@gmail.com>
2021-01-23 22:00:53 +0200

    Change CategoryModelFirebase to add categories using DAO and not invoking listener

--- app/src/main/java/com/example/extrememe/model/category/CategoryModelFirebase.java
+++ app/src/main/java/com/example/extrememe/model/category/CategoryModelFirebase.java
@@ -3,6 +3,7 @@ package com.example.extrememe.model.category;
 import android.util.Log;
 
 import com.example.extrememe.model.Category;
+import com.example.extrememe.model.localDb.CategoryModelSql;
 import com.example.extrememe.services.DatabaseDataLoader;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
@@ -11,8 +12,9 @@ import java.util.List;
 
 public class CategoryModelFirebase {
     private static final String TAG = "CategoryService";
+    private CategoryModelSql categoryModelSql = new CategoryModelSql();
 
-    public void getMemeCategories(final CategoryModel.GetMemeCategoriesListener listener) {
+    public void getMemeCategories() {
         List<Category> list = new ArrayList<>();
 
         DatabaseDataLoader.getDB().collection("categories")
@@ -22,8 +24,7 @@ public class CategoryModelFirebase {
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             list.add(document.toObject(Category.class));
                         }
-
-                        listener.onComplete(list);
+                        categoryModelSql.addCategories(list);
                     } else {
                         Log.w(TAG, "Error getting documents.", task.getException());
                     }
